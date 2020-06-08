4184d8b5f375f1a9ec26bb36e7b78492b6790bf7
Kirill <yakubov.kirill.work@gmail.com>
2020-06-08 11:35:16 +0300

    One

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Messenger</title>
+</head>
+<body>
+  <div id="header">
+    <span>Messages</span>
+  </div>
+  <div id="content">
+    <div id="user-list">
+
+    </div>
+
+    <div id="messages">
+
+    </div>
+  </div>
+
+</body>
+</html>
